8d66af6b2252541e9cab0347e3a0b17105798a62
Xiongzj5 <347964202@qq.com>
2019-06-23 20:25:24 +0800

    add group files

---
+++ models/group.js
@@ -0,0 +1,13 @@
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const GroupSchema = new Schema({
+  name: { type: String },
+  owner: { type: Schema.Types.ObjectId, ref: "User" },
+  members: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  todos: [{ type: Schema.Types.ObjectId, ref: "Todo" }]
+});
+
+module.exports = mongoose.model("Group", GroupSchema);
