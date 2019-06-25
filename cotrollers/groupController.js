ff3dd40d8182e20ccfac24beb97bfb032e8f2060
Xiongzj5 <347964202@qq.com>
2019-06-25 15:10:53 +0800

    groupController.js add function that can return searched group

--- cotrollers/groupController.js
+++ cotrollers/groupController.js
@@ -10,3 +10,13 @@ GroupDAO.prototype.findAll = async function(callback) {
   let groups = await GroupModel.find({}).exec();
   return groups;
 };
+
+/**
+ * @param {String} id group对应的id
+ *
+ * @returns {Object} 返回查找的group
+ */
+GroupDAO.prototype.findGroup = async function(id, callback) {
+  let group = await GroupModel.findById(id).exec();
+  return group;
+};
