0d87a8e31fa6ce255b4c1d59c6c601fb01823b0e
Xiongzj5 <347964202@qq.com>
2019-06-26 10:57:51 +0800

    group.js update about POST request

--- routes/group.js
+++ routes/group.js
@@ -35,3 +35,54 @@ router.get("/:id", async (ctx, next) => {
 
   ctx.body = group;
 });
+
+
+/**
+ *  响应: POST 请求
+ *  API: api.com/group
+ *  作用: 创建新群组
+ *  返回: 新的群组
+ */
+router.post("/", async (ctx, next) => {
+  console.log("API: POST GROUP/");
+
+  const groups_info = ctx.request.body;
+
+  let new_group = await GroupDAO.newGroup(groups_info);
+
+  ctx.body = new_group;
+});
+
+/**
+ *  响应: POST 请求
+ *  API: api.com/group/:id
+ *  作用: 更新群组信息
+ *  返回: 新的群组信息
+ */
+router.post("/:id", async (ctx, next) => {
+  console.log("API: POST GROUP/:id");
+
+  const id = ctx.params.id;
+  const group_info = ctx.request.body;
+
+  let new_group = await GroupDAO.updateGroup(id, group_info);
+
+  ctx.body = new_group;
+});
+
+/**
+ *  响应: POST 请求
+ *  API: api.com/group/:id/user
+ *  作用: 增加群组用户
+ *  返回: 新的群组信息
+ */
+router.post("/:id/user", async (ctx, next) => {
+  console.log("API: POST GROUP/:id/user");
+
+  const id = ctx.params.id;
+  const members = ctx.request.body.members;
+
+  let new_group = await GroupDAO.addMember(id, members);
+
+  ctx.body = new_group;
+});
