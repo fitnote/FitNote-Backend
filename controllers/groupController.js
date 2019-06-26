c4a79adbd463b5fc2a65672e8258329cfe1bb440
cxh666 <Chenxhcxh@163.com>
2019-06-26 14:30:48 +0800

    增加注释

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -3,6 +3,9 @@ const UserDAO = require("./userController");
 
 const GroupDAO = function() {};
 
+/**
+ * @returns {Object} 所有 Groups
+ */
 GroupDAO.prototype.findAll = async function(callback) {
   let groups = await GroupModel.find({}).exec();
   return groups;
