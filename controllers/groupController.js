fe335017d6960e5f24fd7ed8ce3f34683d1d3745
cxh666 <Chenxhcxh@163.com>
2019-06-26 14:29:03 +0800

    增加findAll函数

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -2,3 +2,8 @@ const GroupModel = require("../models/group");
 const UserDAO = require("./userController");
 
 const GroupDAO = function() {};
+
+GroupDAO.prototype.findAll = async function(callback) {
+  let groups = await GroupModel.find({}).exec();
+  return groups;
+};
