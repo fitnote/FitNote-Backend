ab281c9e69566bca028a082ca95fbe3984af4c32
Breeze16 <1121976003@qq.com>
2019-06-26 20:29:04 +0800

    TodoDAO finished

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -4,5 +4,12 @@ const GroupDAO = require("./groupController");
 
 const TodoDAO = function() {};
 
+/**
+ * @returns {Object} 用户的所有 Todos
+ */
+TodoDAO.prototype.findAll = async function(callback) {
+  let todos = await TodoModel.find({}).exec();
+  return todos;
+};
 
 module.exports = new TodoDAO();
